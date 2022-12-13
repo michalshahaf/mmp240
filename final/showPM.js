e57545149c3ac6d982bbf67cfb7fcf353f22f53d
michalshahaf <112114779+michalshahaf@users.noreply.github.com>
2022-12-13 14:44:01 -0500

    Add files via upload

---
+++ final/showPM.js
@@ -0,0 +1,8 @@
+function showPM(){
+        var  menu = document.getElementById("PM");
+        if  (menu.style.display==="none"){
+            menu.style.display="block";
+        } else {
+        menu.style.display="none";
+            }
+        }
